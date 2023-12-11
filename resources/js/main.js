5308d1ba19939540aa5df66efa96fc8cabbf1a8b
Mehmet Akif Güner <mehmetguner058@gmail.com>
2023-12-11 14:39:41 +0300

    Ana sayfa eklemeler

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,5 +1,3 @@
-
-
   $(document).ready(function () {
     $('#menu-toggle').click(function () {
       $('#mobile-menu').toggleClass('menu-open');
@@ -27,6 +25,29 @@
       $(".tab-content > div").addClass("hidden");
       $(target).removeClass("hidden");
     });
+  });
+
+  var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+    breakpoints: {
+      640: {
+        slidesPerView: 2,
+        spaceBetween: 20,
+      },
+      768: {
+        slidesPerView: 3,
+        spaceBetween: 40,
+      },
+      1024: {
+        slidesPerView: 4,
+        spaceBetween: 50,
+      },
+    },
   });
 
 
